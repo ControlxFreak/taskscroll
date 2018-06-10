558d503b041e221e62c18909666d0f6c1629c6fb
unknown <anthony.t.trezza@gmail.com>
2018-06-10 17:51:30 -0400

    Small Updates

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# taskscroll
-Tool for scrolling tasks (awesome for polybar!)
+# TaskScroll
+> Scrolls through your taskwarrior tasks, formats and echos them back to you!  Awesome for polybar! 
+
+![TODO IMAGE](image/todo.png)
+
+## Installation
+
+### Arch Linux Installation
+
+Grab from the ![AUR](todo)
+
+### Other Distro Installation
+
+Clone this repo to your `$XDG_CONFIG_HOME/polybar` folder.
+
+## Polybar Update
+
+To use taskscroll and taskcomplete in polybar, add this to your polybar configuration file.
+
+```ini
+[module/taskscroll]
+interval = 5
+type = custom/script
+exec = taskscroll -v <YOUR DESIRED VERBOSITY LEVEL>
+format = <label>
+format-forground - #FFFF
+format-prefix = "TODO "
+click-left = taskscroll -c
+ ```
+ 
+ *NOTE: If a distrubution other than Arch Linux was used, prepend the directory to taskscroll and taskcomplete (should be `$XDG_CONFIG_HOME/polybar/taskscroll`).* 
+ 
--- taskscroll.sh
+++ taskscroll.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # --------------------------------------------------------------------- #
-# taskscroll 
-#	A handy polybar script that scrolls through your taskwarrior
-#	tasks periodically.
+# taskscroll
+#	Scrolls through your taskwarrior tasks, formats and echos them back 
+#	to you!  Awesome for polybar! 
 #
 # Anthony Trezza
 # 04 June 2018
@@ -35,42 +35,106 @@
 # 
 # Change Log:
 #	- 04 June 2018 trezza taskscroll's birthday <(^.^)>
-#
+#	- 04 June 2018 trezza Updated the location of the data file 
+#				   to standard XDG notation (Issue #3)
+#	- 04 June 2018 trezza added options to allow verbosity level selection
+#				   (Issue #1)
+#	- 04 June 2018 trezza added usage and help
+#	- 04 June 2018 trezza added version number
+#	- 07 June 2018 trezza added -c option for completing the current task
 # TODO:
-#
+#	- 04 June 2018 trezza maybe this should be a format input instead of
+#				   verbosity level? Think about it.
+#	- 04 June 2018 trezza maybe add a -s option for sorting
 # --------------------------------------------------------------------- #
+VERSION=`cat VERSION`
 
-# 0. Define (or create) the configuration file
-if [ ! -z "$XDG_CONFIG_HOME" ]; then
-	CONFDIR="$XDG_CONFIG_HOME/taskscroll"
+# Utility Functions
+function usage() {
+	echo "Usage: "
+	echo "	$0 [-hcv]"
+	echo "	-h		Display help"
+	echo "  -c      Complete the current task"
+	echo "  -v 		Set the verbosity level (default 1)"
+	echo "			0 - Silent Mode (do not echo anything but iterate task data as if it did)"
+	echo "			1 - Description Mode ("Task k: <description>")"
+	echo "			2 - Project Mode ("Task k: <project>:<description>")"
+	echo "			3 - Due Date Mode ("Task k (Due on <date>): <project>:<description>")"
+	echo "			4 - Urgency Mode ("Task k (Due on <date>, Urgency <urgency>: <project>:<description>")"
+	echo "			(where k=task number)"
+} # usage()
+
+function help() {
+	echo "TaskScroll: (Version: $VERSION)"
+	echo "	Scrolls through your taskwarrior tasks, formats and echos them back to you!  Awesome for polybar!"
+	echo""
+	usage()
+} # help()
+
+function getTaskInfo(){
+	task $TASKNUM info | grep "$1" | sed 's/\"$1"   //g'
+} # getTaskInfo()
+
+# Get the input options
+VERBOSITY=1 # Default verbosity to just print the description of the next task
+COMPLETETASK=false
+
+while getopts ":hcv" opt; do
+	case ${opt} in
+		h )
+			help
+			exit 0
+			;;
+		c )
+			COMPLETETASK=true
+			;;
+		v )
+			VERBOSITY=${OPTARG}
+			;;
+		\? )
+			echo "Invalid Input: -$OPTARG"
+			usage
+			exit 1
+			;;
+	esac
+done
+
+shift $((OPTIND -1))
+
+
+# 0. Define (or create) the datafile
+# 04 June 2018 trezza - Fixed location of data file (Issue #3)
+if [ ! -z "$XDG_DATA_HOME" ]; then
+	DATADIR="$XDG_DATA_HOME/taskscroll"
 else
-	CONFDIR="$HOME/.config/taskscroll"
+	DATADIR="$HOME/.local/share/taskscroll"
 fi
 
-CONFILE="$CONFDIR/taskscroll.conf"
-
-if [ ! -d "$CONFDIR" ]; then
-        echo "Creating $CONFDIR"
-        mkdir -p "$CONFDIR"
+if [ ! -d "$DATADIR" ]; then
+        echo "Creating $DATADIR"
+        mkdir -p "$DATADIR"
 fi
 
-if [ ! -f "$CONFILE" ];then
-        echo "Creating $CONFILE"
-        touch "$CONFILE"
+
+DATAFILE="$DATADIR/tasknum"
+
+if [ ! -f "$DATAFILE" ];then
+        echo "Creating $DATAFILE"
+        touch "$DATAFILE"
 fi
 
-# 1. Grab the current task number
-TASKNUM=`cat "$CONFILE"`
+# 1. Grab the current task number, if it is empty, default it to the first task.
+TASKNUM=`cat "$DATAFILE"`
 
-if [ -z "$TASKNUM" ]; then
+if [ -z "$TASKNUM" || "$TASKNUM" -lte 0 ]; then
 	TASKNUM=1
 fi
 
-# 2. Grab the number of tasks
+# 2. Grab the number of tasks, if there are no tasks - echo "No Tasks!"
 NUMTASKS=`task list | tail -1 | sed 's/\ tasks//g'`
 
 if [ "$NUMTASKS" -eq 0 ]; then
-	echo ""
+	echo "No Tasks!"
 	return	
 fi
 
@@ -80,9 +144,42 @@ if [ "$TASKNUM" -gt "$NUMTASKS" ]; then
 fi
 
 # 4. Query taskwarrior and echo the description of the task 
-echo `task $TASKNUM info | grep Description | sed 's/\Description   //g'`
+if [ $COMPLETETASK ]; then 
+
+	task $TASKNUM done
+
+else 
+	PROJECT=getTaskInfo "Project"
+	DESCRIPTION=getTaskInfo "Description"
+	DUE=getTaskInfo "Due"
+	URGENCY=getTaskInfo "Urgency"
+
+	case ${VERBOSITY} in
+		0 )
+			;;
+		1 )
+			echo "Task $TASKNUM: $DESCRIPTION"
+			;;
+		2 )
+			echo "Task $TASKNUM: $PROJECT: $DESCRIPTION"
+			;;
+		3 )
+			echo "Task $TASKNUM (Due: $DUE): $PROJECT: $DESCRIPTION"
+			;;
+		4 )
+			echo "Task $TASKNUM (Due: $DUE, Urgency: $URGENCY): $PROJECT: $DESCRIPTION"
+			;;
+		\? )
+			echo "Unknown Verbosity!"
+			usage
+			exit 1
+			;;
+	esac
+			
+fi
 
 # 5. Update the task number and save it to the configuration file 
-TASKNUM=$(($TASKNUM + 1))
-echo $TASKNUM > $CONFILE
+TASKNUM=$(($TASKNUM +1))
+
+echo $TASKNUM > $DATAFILE
 
